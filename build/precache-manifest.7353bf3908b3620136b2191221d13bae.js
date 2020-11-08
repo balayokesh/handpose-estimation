c6d09f793c03f92e873ae68380759e314937ca1e
Bala Yokesh Mani <balayokeshmani@gmail.com>
2020-11-08 15:17:39 +0530

    Added files via upload

---
+++ build/precache-manifest.7353bf3908b3620136b2191221d13bae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3049eb9f161e08b35bb2d40ddb5519f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a5d4db37db6c398a81",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3b6d2336f46341879b2e",
+    "url": "/static/js/2.21a90696.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/static/js/2.21a90696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a5d4db37db6c398a81",
+    "url": "/static/js/main.5fafcaf2.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "2d839d53d2c375e2aadeac2237b33849",
+    "url": "/static/media/thumbs_up.2d839d53.png"
+  },
+  {
+    "revision": "f4111b8268558f9f1daa3a4184d1f517",
+    "url": "/static/media/victory.f4111b82.png"
+  }
+]);
